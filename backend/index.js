0bfc04dc92c2c7a7b2f8521aace02c2e395ed564
Prashant-sharma-8J <prashantsharmaop@gmail.com>
2026-01-14 11:58:26 +0530

    Delete backend directory

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err.message));
